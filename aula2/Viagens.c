7c60edc741f36b12f5337e2af8d0ee936f612b36
Joao-Pedro <joaopmalvarez@hotmail.com>
2025-08-27 10:05:47 -0300

    Atualizar o Viagens.c

--- aula2/Viagens.c
+++ aula2/Viagens.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
-// tem que fazer o 00:00, a divisao deles, ja fiz a conta
+int minutos() {
+    int min1, min2, hr1, hr2;
+    min1 = 0
+    min2 = 0
+    hr1 = 0
+    hr2 = 0
+        total = horas * 60 + minutos
+}
 
-minutos = 0
-horas = 0
-total = horas * 60 + minutos
 
-int main() {
+
+int diferenca() {
     int inicial, final, tot;
     printf("Informe a hora em que seu voo sai: ", horas, minutos);
-    scanf("%d" inicial);
+    scanf("%d:%d", &hr1, &min1);
+    inicial = hr1 * 60 + min2;
     printf("Informe a hora em que seu voo chega: " horas, minutos);
-    scanf("%d" final);
+    scanf("%d:%d", &hr2, &min2);
+    total = hr2 * 60 + min2
     if (inicial > final) {
         tot = final - inicial + 1440;
         printf("O voo será de %d horas\n", tot);
