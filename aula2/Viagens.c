b411667f96b20e2c0d6445def86e50ec7f11ed50
Joao-Pedro <joaopmalvarez@hotmail.com>
2025-08-27 10:28:26 -0300

    Atualizar o Viagens.c

--- aula2/Viagens.c
+++ aula2/Viagens.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 
-int minutos() {
-    int min1, min2, hr1, hr2, tot;
-    tot = hr1 * 60 + min1;
-    tot = hr2 *60 + min1;
+int diferenca() {
+    int hr1, min1, hr2, min2;
+    int inicial, final, tot;
 
-    int diferenca() {
-        int inicial, final, tot;
-        printf("Informe a hora em que seu voo sai: ");
-        scanf("%d:%d", &hr1, &min1);
-        inicial = hr1 * 60 + min2;
-        printf("Informe a hora em que seu voo chega: ");
-        scanf("%d:%d", &hr2, &min2);
-        final = hr2 * 60 + min2;
-        if (inicial > final) {
-            tot = inicial - final + 1440;
-            printf("O voo será de %d horas\n", tot);
-        }
-        else if (inicial = final) {
-            printf("Os horários dados estão incorretos");
-        }
-        else {
-            tot = final - inicial;
-            printf("O voo será de %d horas\n", tot);
-        }
+    printf("Informe a hora em que seu voo sai (hh:mm): ");
+    scanf("%d:%d", &hr1, &min1);
+    inicial = hr1 * 60 + min1;
+    
+    printf("Informe a hora em que seu voo chega (hh:mm): ");
+    scanf("%d:%d", &hr2, &min2);
+    final = hr2 * 60 + min2;
+
+    if (inicial > final) {
+        tot = (1440 - inicial) + final;
+    } 
+    else if (inicial == final) {
+        printf("Os horários dados estão incorretos\n");
+        return 0;
+    } 
+    else {
+        tot = final - inicial;
     }
+
+    printf("O voo será de %d:%d\n", tot / 60, tot % 60);
+
+    return 0;
+}
+
+int main() {
+    diferenca();
     return 0;
 }
