4c8b6eb636b24d63696527d16dfb48e6e3f60799
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-03-26 12:08:07 +0000

    atualização

--- projetos-quero-adiantar/hannoi/Hannoi.java
+++ projetos-quero-adiantar/hannoi/Hannoi.java
@@ -8,7 +8,17 @@ public class Hannoi {
         Scanner scanner = new Scanner(System.in);
         int numDiscos = scanner.nextInt();
         //adicionar os discos na torre 1
-
+        Comparator<Disco> comparator = Comparator.comparingInt(Disco::getTamanho);
+        Torre<Disco> torre1 = new Torre<>(comparator);
+        for (int i = numDiscos; i >= 1; i--) {
+            torre1.push(new Disco("Disco", i), numDiscos);
+        }
+        //imprimir os discos da torre 1
+        System.out.println("Discos na Torre 1:");
+        Pilha<Disco> fila = new Pilha<>();
+        while (!torre1.isEmpty()) {
+            fila.push(torre1.pop());
+        }   
 
         while (!fila.isEmpty()) {
             System.out.println(fila.pop());
